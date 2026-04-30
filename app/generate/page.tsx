b0ebaaf653878fd5335fc59af4df894a4eded554
yueyue-bird <3766820830@qq.com>
2026-04-30 21:00:11 +0800

    go

--- app/generate/page.tsx
+++ app/generate/page.tsx
@@ -1,6 +1,6 @@
 // app/generate/page.tsx
 'use client';
-
+export const dynamic = 'force-dynamic';
 import { useState, useEffect } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 
