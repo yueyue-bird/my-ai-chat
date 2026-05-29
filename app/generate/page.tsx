16fa7b829777942ad6ef4d3cf62272a357b014e4
yueyue-bird <3766820830@qq.com>
2026-05-29 17:20:09 +0800

    Fix generate content import casing

--- app/generate/page.tsx
+++ app/generate/page.tsx
@@ -1,6 +1,6 @@
 // app/generate/page.tsx
 import { Suspense } from 'react';
-import GenerateContent from './GenerateContent';
+import GenerateContent from './generateContent';
 
 export default function GeneratePage() {
   return (
